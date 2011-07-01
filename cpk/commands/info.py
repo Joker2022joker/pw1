861a44d465225ed598d48c25a817bc5b0d406416
yac <yac@blesmrt.net>
2011-07-02 01:19:38 +0200

    + print config

--- cpk/commands/info.py
+++ cpk/commands/info.py
@@ -6,3 +6,10 @@ from . import Command as IFace
 class Command(IFace):
     def _run(self,args):
         print "db:\t{0}".format(self.app._get_db())
+
+        print "config:"
+        for s in self.conf.sections():
+            print "\t%s" % s
+
+            for i in self.conf.items(s):
+                print "\t\t%s = %s" % i
