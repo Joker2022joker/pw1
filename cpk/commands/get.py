d78f32a7412a3f112c52499404b83563b4f34379
yac <yac@blesmrt.net>
2011-07-02 02:44:18 +0200

    - hardcoded password attribute

--- cpk/commands/get.py
+++ cpk/commands/get.py
@@ -3,6 +3,7 @@
 
 from . import Command as IFace
 from model import Attribute, Node
+from sqlalchemy.orm.exc import NoResultFound
 
 class Command(IFace):
     def attribute(self):
@@ -16,9 +17,16 @@ class Command(IFace):
         filters = self.tokens_2_filters(self.tokenize_nodes())
 
         if filters[-1]['node']:
-            filters.append({'attr':'password'})
-            # ^ FIXME: hardcoded
-            # append filter for goal we want to retrieve unless it has been specified by the user as last node
+            try:
+                a = Attribute.password()
+                filters.append({'attr':a.name})
+                # append filter for goal we want to retrieve
+                # unless it has been specified by the user as the last node
+                # or there is no configured password attribute
+
+            except NoResultFound:
+                filters.append({})
+                # append empty filter so we'll get the "one more" node
 
         goal = Node.get(filters)
 
