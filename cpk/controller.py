9bc1b981a1a932b72a339c06c20f23709605ea61
Jan (yac) Matějka <JMatejka@suse.cz>
2013-04-08 14:39:32 +0200

    use -h as default

--- cpk/controller.py
+++ cpk/controller.py
@@ -3,13 +3,18 @@
 
 from __future__ import print_function
 from cement.core import foundation, handler
-from cement.core.controller import CementBaseController, expose
+from cement.core.controller import CementBaseController, expose, IController
 from datetime import datetime
 
 class CPKController(CementBaseController):
     class Meta:
         label = 'base'
         description = 'CPK entry point'
+
+    @expose()
+    def default(self):
+        self.app._meta.argv = ['-h']
+        self._dispatch()
 
 class NewController(CementBaseController):
     @expose
