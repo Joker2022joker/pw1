01d81c5964ff3727c794c219a0dec3eb13e7aa0c
Jan (yac) Matějka <JMatejka@suse.cz>
2013-04-08 16:13:59 +0200

    Do not use raw assert
    * it does not display the values that were asserted when failure happens

--- tests/utils.py
+++ tests/utils.py
@@ -8,16 +8,17 @@ from cpk.utils import tokenize_assignments, ctrls
 from unittest import TestCase
 
 from cement.core.controller import CementBaseController
+from nose.tools import eq_
 
 def test_ctrls():
     class Ctrl(CementBaseController):
         pass
 
     input_ = [CementBaseController, Ctrl, 1, "foo"]
-    assert ctrls(input_) == [Ctrl]
+    eq_(ctrls(input_), [Ctrl])
 
 def test_tokenize_assignments():
     input_ = ['aZ0=b.,:/', 'x=d=e']
     output = [['aZ0', 'b.,:/'], ['x', 'd=e']]
 
-    assert tokenize_assignments(input_) == output
+    eq_(tokenize_assignments(input_), output)
