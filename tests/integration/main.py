8ce85716a323378c060297e883cf3cf8a3a6dc3a
Jan (yac) Matějka <JMatejka@suse.cz>
2013-04-11 14:03:26 +0200

    more checks in intgr. wallet_open

--- tests/integration/main.py
+++ tests/integration/main.py
@@ -9,7 +9,7 @@ from nose.tools import eq_, ok_, raises
 os.environ['XDG_DATA_HOME'] = dirname(abspath(__file__))
 # NOTE: this seems to NEED to be set up before the xdg module is loaded
 
-from cpk.wallet import Wallet
+from cpk.wallet import Wallet, Service, Record
 from cpk.crypto import Dummy
 
 def test_wallet_open():
@@ -17,3 +17,9 @@ def test_wallet_open():
     w = Wallet.open(wfile, Dummy())
     eq_(len(w.records), 1)
     eq_(len(w.services.keys()), 1)
+
+    s = Service('www', ['host', 'user'], ['pass'])
+    eq_(w.services['www'], s)
+
+    r = Record(s, host='example.com', user='laika', **{'pass':'nbusr123'})
+    eq_(w.records[0], r)
